fbcd6cd0261e79d71a9269de399f6b5446ba006b
Lê Nguyễn Bảo Anh <anhlenguyenbaoanh@gmail.com>
2025-05-02 21:39:41 +0700

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FruitEshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
